2ebac2dc3ffda378d54cd28d62ac46c0af0c23ea
Andrew Berezovskyi <andriib@kth.se>
2026-09-22 21:32:14 +0200

    style: remove unused usings from XSS preview test

--- src/StrictDocOslcRmServer/StrictDocOslcRm.Tests/RequirementControllerTests.cs
+++ src/StrictDocOslcRmServer/StrictDocOslcRm.Tests/RequirementControllerTests.cs
@@ -1,11 +1,5 @@
-using System.IO;
-using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Mvc.ViewEngines;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
 using OSLC4Net.Domains.RequirementsManagement;
